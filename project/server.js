e9ffd29b4a43d2b5e69b021b317666c6e1af8f97
KevinDon <kevintang002@gmail.com>
2018-05-04 15:13:03 +0800

    更新每日修改的NodeJS代码

--- project/server.js
+++ project/server.js
@@ -7,9 +7,10 @@ http.createServer(function (request, response) {
     // 内容类型: text/plain
     response.writeHead(200, {'Content-Type': 'text/plain'});
 
+
     // 发送响应数据 "Hello World"
     response.end('Hello World\n');
-}).listen(8888);
+}).listen(8008);
 
 // 终端打印如下信息
 console.log('Server running at http://127.0.0.1:8888/');
