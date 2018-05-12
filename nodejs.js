37efed574b6e8accb98a94f2b12fb78c657fc181
Kevin Don <kevintang002@gmail.com>
2018-05-12 15:22:41 +0800

    修改冲突问题文件

--- nodejs.js
+++ nodejs.js
@@ -1,12 +1,26 @@
-console.log("Hello World");
-console.log("Hello World");
-console.log("Hello World");
-console.log("Hello World");
-console.log("Hello World");
-console.log("Hello World");
-console.log("Hello World");
-console.log("Hello World");
-console.log("Hello World");
-console.log("Hello World");
-console.log("Hello World");
-console.log("Hello World");
+var http = require('http');
+var express = require('express');
+var app = express();
+
+http.createServer(function (request, response) {
+
+    // 发送 HTTP 头部
+    // HTTP 状态值: 200 : OK
+    // 内容类型: text/plain
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    // var postData = '';
+    // request.setEncoding('utf8');
+    // request.on('data', function(chunk){
+    //     postData += chunk;
+    // })
+    // //侦听请求的end事件
+    // request.on('end', function(){
+    //     response.end(postData);
+    // })
+
+    // 发送响应数据 "Hello World"
+    response.end('Hello World\n');
+}).listen(8008);
+
+// 终端打印如下信息
+console.log('Server running at http://127.0.0.1:8008/');
